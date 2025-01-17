3ca5c335d246038cf641f5ca1581d4b333c127b9
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2025-01-18 01:03:28 +0530

    23

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar.jsx'
 import Hero from '../components/Hero.jsx'
-import JobListing from '../components/jobListing.jsx'
+import JobListing from '../../../client/src/components/JobListing.jsx'
 import AppDownload from '../components/AppDownload.jsx'
 import Footer from '../components/Footer.jsx'
 
